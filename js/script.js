4f954d8a912457384a66533b964bd0be800c3deb
Anna <null>
2021-03-02 14:28:15 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing:'linear',
+        infinite: true,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        draggable: true,
+        swipe: true,
+        touchThreshold: 3,
+        touchMove: true,
+        waitForAnimate: false,
+        centerMode: false,
+        variableWidth: false,
+        rows: 1,
+        slidesPerRow: 1,
+        vertical: false,
+        verticalSwiping: false,
+
+    });
+})
